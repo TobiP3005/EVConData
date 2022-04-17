343a435d6a8776f71ea166a98706e62d8addf622
Tobias Pfatrisch <tobias.pfatrisch@adac.de>
2022-04-17 23:31:50 +0200

    Update Consumption model

--- app/src/main/java/com/example/evcondata/model/Consumption.kt
+++ app/src/main/java/com/example/evcondata/model/Consumption.kt
@@ -1,10 +1,17 @@
 package com.example.evcondata.model
 
+interface ModelDTO<T> {
+    var item: T
+}
+
+class ConsumptionModelDTO(override var item: Consumption) : ModelDTO<Consumption>
+
 data class Consumption (
     var name: String?,
-            var distance: Int?,
-            var altitudeUp: Int?,
-            var altitudeDown: Int?,
-            var consumption: Float?,
-            var temperature: Int?
-        )
+    var distance: Float?,
+    var consumption: Float?,
+    var temperature: Int?,
+    var altitudeUp: Int?,
+    var altitudeDown: Int?,
+    var type: String = "consumption"
+)
