d546d90fe350e13ce4254be9360fbcdc57043de4
Tobias Pfatrisch <tobias.pfatrisch@adac.de>
2022-04-22 18:01:05 +0200

    Implement Car list fragment with sample data #13

--- app/src/main/java/com/example/evcondata/MainActivity.kt
+++ app/src/main/java/com/example/evcondata/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_my_car, R.id.myConsumptionFragment, R.id.mapsFragment
+                R.id.nav_my_car, R.id.myConsumptionFragment, R.id.mapsFragment, R.id.carFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
