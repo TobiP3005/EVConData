50e6a2e65eb430baf6c365b8d2367d4ebdade39e
Tobias Pfatrisch <tobias.pfatrisch@adac.de>
2022-07-14 11:44:00 +0200

    reformat code

--- app/src/main/java/com/example/evcondata/ui/myCar/MyCarFragment.kt
+++ app/src/main/java/com/example/evcondata/ui/myCar/MyCarFragment.kt
@@ -7,14 +7,12 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.coroutineScope
 import com.example.evcondata.databinding.FragmentMyCarBinding
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -25,34 +23,55 @@ class MyCarFragment : Fragment() {
     private val myCarViewModel: MyCarViewModel by viewModels()
     private lateinit var binding: FragmentMyCarBinding
     private var myCar: String? = null
+    private lateinit var adapter: ArrayAdapter<String>
 
-    @OptIn(InternalCoroutinesApi::class)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMyCarBinding.inflate(layoutInflater)
-
         binding = FragmentMyCarBinding.inflate(inflater, container, false)
 
-        val adapter = ArrayAdapter(requireContext(),
+        adapter = ArrayAdapter(
+            requireContext(),
             android.R.layout.simple_spinner_dropdown_item, mutableListOf<String>()
         )
         adapter.insert("", 0)
         binding.spinnerCars.adapter = adapter
 
         var check = 0
-        binding.spinnerCars.onItemSelectedListener = object : OnItemSelectedListener{
+        binding.spinnerCars.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                if(++check > 1) {
-                    val t = p0!!.getItemAtPosition(p2).toString()
-                    myCarViewModel.setMyCar(t)
+                if (++check > 1) {
+                    val car = p0!!.getItemAtPosition(p2).toString()
+                    myCarViewModel.setMyCar(car)
+                    loadData(car)
                 }
             }
-            override fun onNothingSelected(p0: AdapterView<*>?){}
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
 
+        keepCarListUpToDate()
+        keepMyCarUpToDate()
+
+        return binding.root
+    }
+
+    private fun keepMyCarUpToDate() {
+        lifecycle.coroutineScope.launch {
+            myCarViewModel.myCarFlow
+                .collect { carName ->
+                    myCar = carName
+                    val spinnerPosition = adapter.getPosition(carName)
+                    binding.spinnerCars.setSelection(spinnerPosition)
+                    loadData(carName)
+                }
+        }
+    }
+
+    private fun keepCarListUpToDate() {
         lifecycle.coroutineScope.launch {
             myCarViewModel.carNames.collect { carNameList ->
                 adapter.clear()
@@ -67,31 +86,32 @@ class MyCarFragment : Fragment() {
                 }
             }
         }
-
-        lifecycle.coroutineScope.launch {
-            myCarViewModel.myCarFlow
-                .collect { carName ->
-                    myCar = carName
-                    val spinnerPosition = adapter.getPosition(carName)
-                    binding.spinnerCars.setSelection(spinnerPosition)
-                    loadData(carName)
-                }
-        }
-
-        return binding.root
     }
 
-    private fun loadData(carName: String){
-        val car = myCarViewModel.myCar(carName)
+    private fun loadData(carName: String) {
+        val car = myCarViewModel.getCar(carName)
 
-        if (car != null){
-            binding.itemPower.text = String.format("%s kw", if (car.power == null) "?" else car.power.toString())
-            binding.itemSpeed.text = String.format("%s kmh", if (car.maxSpeed == null) "?" else car.maxSpeed.toString())
-            binding.itemWltpRange.text = String.format("%s km", if (car.wltpRange == null) "?" else car.wltpRange.toString())
-            binding.itemWltpConsumption.text = String.format("%s kwh", if (car.wltpConsumption == null) "?" else car.wltpConsumption.toString())
-            binding.itemBattery.text = String.format("%s kwh", if (car.battery == null) "?" else car.battery.toString())
-            binding.itemChargeSpeed.text = String.format("%s kw", if (car.chargeSpeed == null) "?" else car.chargeSpeed.toString())
-            binding.itemAcceleration.text = String.format("%s sec", if (car.acceleration == null) "?" else car.acceleration.toString())
+        if (car != null) {
+            binding.itemPower.text =
+                String.format("%s kw", if (car.power == null) "?" else car.power.toString())
+            binding.itemSpeed.text =
+                String.format("%s kmh", if (car.maxSpeed == null) "?" else car.maxSpeed.toString())
+            binding.itemWltpRange.text =
+                String.format("%s km", if (car.wltpRange == null) "?" else car.wltpRange.toString())
+            binding.itemWltpConsumption.text = String.format(
+                "%s kwh",
+                if (car.wltpConsumption == null) "?" else car.wltpConsumption.toString()
+            )
+            binding.itemBattery.text =
+                String.format("%s kwh", if (car.battery == null) "?" else car.battery.toString())
+            binding.itemChargeSpeed.text = String.format(
+                "%s kw",
+                if (car.chargeSpeed == null) "?" else car.chargeSpeed.toString()
+            )
+            binding.itemAcceleration.text = String.format(
+                "%s sec",
+                if (car.acceleration == null) "?" else car.acceleration.toString()
+            )
 
             Picasso.get().load(car.imageUri).into(binding.itemCarImage)
         }
@@ -99,7 +119,8 @@ class MyCarFragment : Fragment() {
         lifecycle.coroutineScope.launch {
             myCarViewModel.getAvgConsumption(carName)
                 .collect { avg ->
-                    binding.itemCommunityConsumption.text = String.format("%s kwh",
+                    binding.itemCommunityConsumption.text = String.format(
+                        "%s kwh",
                         avg?.toString() ?: "?"
                     )
                 }
@@ -108,7 +129,8 @@ class MyCarFragment : Fragment() {
         lifecycle.coroutineScope.launch {
             myCarViewModel.getMyConsumption(carName)
                 .collect { avg ->
-                    binding.itemMyConsumption.text = String.format("%s kwh",
+                    binding.itemMyConsumption.text = String.format(
+                        "%s kwh",
                         avg?.toString() ?: "?"
                     )
                 }
--- app/src/main/java/com/example/evcondata/ui/myCar/MyCarViewModel.kt
+++ app/src/main/java/com/example/evcondata/ui/myCar/MyCarViewModel.kt
@@ -4,24 +4,27 @@ import androidx.lifecycle.ViewModel
 import com.example.evcondata.data.auth.UserPreferencesRepository
 import com.example.evcondata.data.car.CarRepository
 import com.example.evcondata.data.consumption.ConsumptionRepository
+import com.example.evcondata.data.location.LocationRepository
 import com.example.evcondata.model.Car
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class MyCarViewModel @Inject constructor (
+class MyCarViewModel @Inject constructor(
     private val carRepository: CarRepository,
     private val consumptionRepository: ConsumptionRepository,
     private val locationRepository: LocationRepository,
     userPreferencesRepository: UserPreferencesRepository
-)
-    : ViewModel() {
+) : ViewModel() {
 
     val userPref = userPreferencesRepository
 
-    val myCar: (String) -> Car? = { carName: String ->
-        carRepository.getMyCar(carName)
+    val myCar: Car?
+        get() = carRepository.getMyCar()
+
+    val getCar: (String) -> Car? = { carName: String ->
+        carRepository.getCar(carName)
     }
 
     val myCarFlow: Flow<String>
